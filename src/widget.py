5a03cfa0b1192ae62914d78c6b97b4f22c92c302
Afanasy <128970784+hvdxxx@users.noreply.github.com>
2026-08-22 19:39:17 +0900

    Final

--- src/widget.py
+++ src/widget.py
@@ -26,3 +26,7 @@ def get_date(date_string: str) -> str:
     return f"{day}.{month}.{year}"
 
 print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("Visa Classic 6831982476737658"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(get_date("2024-03-11T02:26:18.671407"))
